750e5c6d8c86575548e9fb97f7a86f4eff167c7c
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2013-04-17 16:33:20 -0400

    big update

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,7 +44,20 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_title("About")
     end
+  end
+    
+    # contact page
+    describe "Contact page" do
+    
+      it "should have the content 'Contact'" do
+        visit "/static_pages/contact"
+        expect(page).to have_content("Contact")
+      end
+    
+      it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_title("Contact")
+      end
+    end
     
   end
-  
-end
