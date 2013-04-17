7f66f1cd437e1df5d01481fbd35b4015a3e85d79
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2013-04-17 14:29:55 -0400

    static to dynamic

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  
+  # home page
+  describe "Home page" do
+    
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_content("Home")
+    end  
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+    
+  end
+  
+  # help page
+  describe "Help page" do
+    
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content("Help")
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+    
+  end
+  
+  # about page
+  describe "About page" do
+    
+    it "should have the content 'About'" do
+      visit "/static_pages/about"
+      expect(page).to have_content("About")
+    end
+    
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
+    end
+    
+  end
+  
+end
